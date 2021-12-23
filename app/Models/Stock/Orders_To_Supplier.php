900a4ba898922be7d7101cb594d9f11f3972b37c
Dion Nguyen <dion.nguyen77000@gmail.com>
2021-12-23 16:35:19 +1000

    finish order kitchen section

--- app/Models/Stock/Orders_To_Supplier.php
+++ app/Models/Stock/Orders_To_Supplier.php
@@ -13,21 +13,26 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Orders_To_Supplier extends Model
 {
-    protected $table = 'orders_to_suppliers';
-    use HasFactory  ;
-    // LogsActivity
-    // protected static $logAttributes = ['user', 'supplier','estimated_price','ordered_date','Note'];
-    
-    // protected $appends = ['InvNumber'];
-
-    protected $fillable = [
-       
-     ];
 
      
-
-   
+     protected $table = 'orders_to_suppliers';
+     use HasFactory  ;
+     // LogsActivity
+     // protected static $logAttributes = ['user', 'supplier','estimated_price','ordered_date','Note'];
+     
+     // protected $appends = ['InvNumber'];
  
-   
+     protected $fillable = [
+        
+      ];
+ 
+      
+ 
+      public function invoices_from_supplier()
+      {
+          return $this->belongsTo(Invoices_From_Supplier::class,'invoices_from_supplier_id');
+      }
+  
+
     
 }
